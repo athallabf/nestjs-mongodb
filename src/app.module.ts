f0683f2507d58420196b98bdc4c5f8562472a6d1
AthallaITS <5027231018@student.its.ac.id>
2024-02-28 08:06:01 +0700

    feat(repo): add abstract repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DB_URI)],
+  imports: [MongooseModule.forRoot(process.env.DB_URI), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
